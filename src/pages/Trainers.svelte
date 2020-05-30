9feca591e30e7cef7c85353b5c80eec8beb313f8
gentilmente <egentilemontes@gmail.com>
2020-05-29 23:19:50 -0300

    added new and delete trainer

--- src/pages/Trainers.svelte
+++ src/pages/Trainers.svelte
@@ -3,9 +3,40 @@
   import Trainer from "../components/Trainer.svelte";
   // define the data holding variable
   let trainers;
+  var url = "https://api.dev.perfivo.com/pokeapi/v0/trainers/";
+
+  let query = "";
+  let id = "";
+  let result;
+
+  async function setNewTrainer() {
+    await fetch(url, {
+      method: "POST", // or 'PUT'
+      body: JSON.stringify({ name: query }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .catch(error => console.error("Error:", error))
+      .then(response => console.log("Success:", response));
+  }
+
+  async function deleteTrainer() {
+    await fetch(url + id.trim() + "/", {
+      method: "DELETE", // or 'PUT'
+      body: JSON.stringify({ id: id.trim() }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .catch(error => console.error("Error:", error))
+      .then(response => console.log("Success:", response));
+  }
 
   onMount(async () => {
-    await fetch(`https://api.dev.perfivo.com/pokeapi/v0/trainers/`)
+    await fetch(url)
       .then(r => r.json())
       .then(data => {
         trainers = data;
@@ -14,6 +45,10 @@
 </script>
 
 <style>
+  form {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
   .loading {
     opacity: 0;
     animation: 0.4s 0.8s forwards fade-in;
@@ -27,11 +62,22 @@
     }
   }
   ul {
-    list-style-type: none;   
+    list-style-type: none;
   }
 </style>
 
 <h2>Trainers</h2>
+
+<form class="form-inline" on:submit|preventDefault={setNewTrainer}>
+  <input class="w-75 form-control" placeholder="name" bind:value={query} />
+  <button class="w-25 btn btn-dark">create new</button>
+</form>
+
+<form class="form-inline" on:submit|preventDefault={deleteTrainer}>
+  <input class="w-75 form-control" placeholder="id" bind:value={id} />
+  <button class="w-25 btn btn-danger">delete</button>
+</form>
+
 {#if trainers}
   <ul>
     {#each trainers as trainer}
